ba65716b7f5811df4479a1ef86a72d4ef8ec28c4
pedrix-48 <155636643+pedrix-48@users.noreply.github.com>
2024-11-08 22:17:45 +0900

    konek databse ba mysql

--- t_library/settings.py
+++ t_library/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 't_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libarydb',
+        'USER': 'root',
+        'PASSWORD': 'frieren',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
