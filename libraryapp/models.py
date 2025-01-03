14cac3c6717205382eeb8411495ef5be0d912733
pedrix-48 <155636643+pedrix-48@users.noreply.github.com>
2025-01-04 00:00:01 +0900

    kria admin user model

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
+class Admin_user(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.username
 class Departamento(models.Model):
     id_dep = models.CharField(max_length=5, primary_key=True)
     naran_dep = models.CharField(max_length=50)
