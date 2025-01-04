b26156d8781abbf674d5432f473d666cd713da73
pedrix-48 <155636643+pedrix-48@users.noreply.github.com>
2025-01-04 12:58:06 +0900

    fix some minor bugs

--- t_library/settings.py
+++ t_library/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
